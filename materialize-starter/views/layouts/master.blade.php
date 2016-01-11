7ba0d397685c8c9c8834bc8ef851106b2f54cf1d
Fatih Kececi <fatih.kececi@yahoo.co.uk>
2016-01-11 08:14:40 +0200

    Materialize Starter Theme for Agola Platform

---
+++ materialize-starter/views/layouts/master.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+
+    <meta name="_token" content="{{ csrf_token() }}"/>
+    <title>@yield('title', site_name())</title>
+    <meta name="description" content="@yield('meta_description')">
+    <meta name="keywords" content="@yield('meta_keywords')">
+    @yield('meta')
+
+    <!-- CSS  -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="{{ Theme::asset('css/materialize.min.css') }}" type="text/css" rel="stylesheet"
+          media="screen,projection"/>
+    <link href="{{ Theme::asset('css/style.css') }}" type="text/css" rel="stylesheet" media="screen,projection"/>
+    @yield('header-styles')
+
+</head>
+<body>
+
+{{-- header --}}
+@include('materialize-starter::partials.header')
+
+{{-- içerik --}}
+@yield('content')
+
+{{-- footer --}}
+@include('materialize-starter::partials.footer')
+
+
+@yield('footer-styles')
+
+@yield('footer-scripts')
+
+<script src="{{ Theme::asset('js/jquery-2.2.0.min.js') }}"></script>
+<script src="{{ Theme::asset('js/materialize.min.js') }}"></script>
+<script src="{{ Theme::asset('js/init.js') }}"></script>
+
+{{-- google analytics --}}
+@yield('google_analytics', googleAnalytics())
+
+</body>
+</html>
+
---
+++ materialize-starter/views/partials/header.blade.php
@@ -0,0 +1,38 @@
+{{-- header alanı --}}
+<nav class="white" role="navigation">
+    <div class="nav-wrapper container">
+        <a id="logo-container" href="{{ home_url() }}" class="brand-logo">
+            @if(site_logo())
+                <img src="{{ site_logo() }}" class="ui small image">
+            @else
+                {{ site_name() }}
+            @endif
+        </a>
+        <ul class="right hide-on-med-and-down">
+            <li><a href="#">Navbar Link</a></li>
+        </ul>
+
+        <ul id="nav-mobile" class="side-nav">
+            <li><a href="#">Navbar Link</a></li>
+        </ul>
+        <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+    </div>
+</nav>
+
+<div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+        <div class="container">
+            <br><br>
+            <h1 class="header center teal-text text-lighten-2">{{ site_name() }}</h1>
+            <div class="row center">
+                <h5 class="header col s12 light">{{ site_description() }}</h5>
+            </div>
+            <div class="row center">
+                <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">Get Started</a>
+            </div>
+            <br><br>
+
+        </div>
+    </div>
+    <div class="parallax"><img src="{{ Theme::asset('images/background1.jpg') }}" alt="Unsplashed background img 1"></div>
+</div>
