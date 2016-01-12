b9a54d8d53140e7ef787c1b8c4cb4c161950b49a
Fatih Kececi <fatih.kececi@yahoo.co.uk>
2016-01-12 02:58:17 +0200

    Boilerplate Theme for Agola Platform

---
+++ boilerplate/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends($theme_layout)
+@section('meta_description'){{ meta_description() }}@stop
+@section('meta_keywords'){{ meta_keywords() }}@stop
+
+@section('content')
+
+@stop
---
+++ boilerplate/views/layouts/master.blade.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        {{-- meta --}}
+        <meta charset="utf-8"/>
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>@yield('title', site_name())</title>
+        <meta name="description" content="@yield('meta_description')">
+        <meta name="keywords" content="@yield('meta_keywords')">
+        @yield('meta')
+
+        {{-- header stylesheets --}}
+        <link rel="stylesheet" type="text/css" href="{{ Theme::asset('css/site.css') }}">
+        @yield('header-styles')
+
+        {{-- header scripts --}}
+        <script src="{{ Theme::asset('js/site.js') }}"></script>
+        @yield('header-scripts')
+
+    </head>
+<body>
+
+{{-- header --}}
+@include('boilerplate::partials.header')
+
+{{-- içerik --}}
+@yield('content')
+
+{{-- footer --}}
+@include('boilerplate::partials.footer')
+
+{{-- footer stylesheets --}}
+@yield('footer-styles')
+
+{{-- footer scripts --}}
+@yield('footer-scripts')
+
+{{-- google analytics --}}
+@yield('google_analytics', googleAnalytics())
+
+</body>
+</html>
+
