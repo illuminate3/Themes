fc514cad03e54a4ca57c04d0a4cde9f0b2aa2a24
Fatih Kececi <fatih.kececi@yahoo.co.uk>
2016-03-09 04:53:44 +0200

    Updated components.php

--- boilerplate/components.php
+++ boilerplate/components.php
@@ -1,1 +1,18 @@
 <?php 
+/**
+ * @example @component_pageheader('Sayfa Başlığı')
+ */
+/*
+\Component::register('pageheader', function($title) {
+    return "<h1 class=\"page-header\">{$title}</h1>";
+});
+*/
+
+/**
+ * @example @component_modal('Başlık', 'Modal İçerik')
+ */
+/*
+\Component::register('modal', function($title, $content) {
+    return \Theme::view('partials.components.modal', compact('title', 'content'));
+});
+*/
